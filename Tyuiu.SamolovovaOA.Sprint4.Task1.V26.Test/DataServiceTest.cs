45494e14a525384eb78c12f9ee1eeb85c1ecea95
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-11-05 14:08:38 +0500

    Task1

---
+++ Tyuiu.SamolovovaOA.Sprint4.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.SamolovovaOA.Sprint4.Task1.V26.Lib;
+namespace Tyuiu.SamolovovaOA.Sprint4.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+               int[] array = { 7, 7, 8, 8, 2, 6, 3, 3, 1, 1 };
+
+               int result = ds.Calculate(array);
+
+               int wait = 22; 
+
+               Assert.AreEqual(wait, result);
+            
+
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint4.Task1.V26/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.SamolovovaOA.Sprint4.Task1.V26.Lib;
+namespace Tyuiu.SamolovovaOA.Sprint4.Task1.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[] array = new int[10];
+
+            Console.WriteLine("Введите 10 чисел от 0 до 8:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int result = ds.Calculate(array);
+            Console.WriteLine($"Сумма нечетных элементов массива = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
