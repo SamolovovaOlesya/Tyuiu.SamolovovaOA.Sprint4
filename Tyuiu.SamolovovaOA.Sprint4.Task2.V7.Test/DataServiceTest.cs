32c4ace7fed750e3b065d983aa9effbaf8d3a07c
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-11-05 14:27:59 +0500

    Task2

---
+++ Tyuiu.SamolovovaOA.Sprint4.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SamolovovaOA.Sprint4.Task2.V7.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint4.Task2.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 5, 6, 7, 8, 9, 4, 4, 6, 5, 8, 9 };
+
+            int result = ds.Calculate(array);
+
+            int wait = 4 + 6 + 8 + 4 + 4 + 6 + 8; 
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint4.Task2.V7/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.SamolovovaOA.Sprint4.Task2.V7.Lib;
+namespace Tyuiu.SamolovovaOA.Sprint4.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rand = new Random();
+
+            int[] array = new int[12];
+
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(4, 10); 
+                Console.Write(array[i] + " ");
+            }
+
+            int result = ds.Calculate(array);
+
+            Console.WriteLine("\nСумма чётных элементов = " + result);
+            Console.ReadKey();
+        }
+    }
+}
